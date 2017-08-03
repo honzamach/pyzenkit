73d9c6b3f28f12968e0480f5aa1e604ce30c0e19
Honza Mach <honza.mach.ml@gmail.com>
2017-08-03 09:28:25 +0200

    Added correct copyright and license header to auxiliary project  files.

--- Makefile
+++ Makefile
@@ -1,3 +1,14 @@
+#-------------------------------------------------------------------------------
+# This file is part of PyZenKit package.
+#
+# Copyright (C) since 2016 CESNET, z.s.p.o (http://www.ces.net/)
+# Copyright (C) since 2015 Jan Mach <honza.mach.ml@gmail.com>
+# Use of this package is governed by the MIT license, see LICENSE file.
+#
+# This project was initially written for personal use of the original author. Later
+# it was developed much further and used for project of author`s employer.
+#-------------------------------------------------------------------------------
+
 #
 # Minimal makefile for Sphinx documentation
 #
--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 #-------------------------------------------------------------------------------
-# Copyright (C) since 2016 Jan Mach <email@jan-mach.cz>
-# Use of this source is governed by the MIT license, see LICENSE file.
+# This file is part of PyZenKit package.
+#
+# Copyright (C) since 2016 CESNET, z.s.p.o (http://www.ces.net/)
+# Copyright (C) since 2015 Jan Mach <honza.mach.ml@gmail.com>
+# Use of this package is governed by the MIT license, see LICENSE file.
+#
+# This project was initially written for personal use of the original author. Later
+# it was developed much further and used for project of author`s employer.
 #-------------------------------------------------------------------------------
 
 # Resources:
