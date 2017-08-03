c561146e512718d3e59b5bb3b52906393cad3293
Honza Mach <honza.mach.ml@gmail.com>
2017-08-03 09:45:51 +0200

    Increased package version and deployed package to PyPI.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name = 'pyzenkit',
-    version = '0.32',
+    version = '0.33',
     description = 'Python 3 script and daemon toolkit',
     long_description = long_description,
     classifiers = [
