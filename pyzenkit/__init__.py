236a4c70537f8b8c42f806c123e5c8ef951fa423
Honza Mach <honza.mach.ml@gmail.com>
2018-02-16 17:54:52 +0100

    DEPLOY: Increased package version to '0.41' and deployed package to PyPI.

--- pyzenkit/__init__.py
+++ pyzenkit/__init__.py
@@ -11,4 +11,4 @@
 # it was developed much further and used for project of author`s employer.
 #-------------------------------------------------------------------------------
 
-__version__ = "0.40"
+__version__ = "0.41"
